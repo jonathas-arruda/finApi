5fca72701aa084e819fbd1f010bf4eedc72a2c7e
Jonathas <jonathasarruda@outlook.com>
2022-09-27 16:05:26 -0300

    Validating the existence of the account to extract bank statement.

--- src/index.js
+++ src/index.js
@@ -15,15 +15,26 @@ app.post("/account", (request, response) => {
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf );
 
   if (customerAlreadyExists) {
-    return response.status(400).json({ error: "Customer Already Exists!" });
+    return response.status(400).json({ error: "Customer already exists!" });
   }
 
   customers.push({
+    id:uuidv4(),
     cpf,
     name,
-    id:uuidv4(),
     statement: [],
   });
-  console.log(customers)
   return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+  
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  return response.json(customer.statement)
 });
